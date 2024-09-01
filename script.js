c21fc40214a2c939c48e717dc3b2fee1eea89606
kamerligulsum <103079330+kamerligulsum@users.noreply.github.com>
2024-09-01 13:12:35 +0300

    Add files via upload
    değişken ve sabit kavramları

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
-// if koşul ifadesi 
+//var let ve const ile değişken tanımlanabilir
 
-/* if bir değer ya da değişken true olduğunda ona göre koşul atamamızı sağlar */
+//her işlemin sonunda noktalı vilgül koymak isteğe bağlıdır.konulmadığı takdirde de kabul edilir
 
-let benimYasim = 18
-let onunYasi = 25
+let yasim = 18; // bu şekilde değiştirilebilir bir değişken tanımlarız
 
-/* burda benim yaşımın onunkinden küçük olup olmadığını sordum ve cevap doğru geldiğinde ise o benden büyük yazısını yazdırmasını istedim */
-if (benimYasim > onunYasi) {
-    console.log("o benden büyük");
-}
+console.log(yasim); //burda verdiğimiz değişken ekrana gelir. değişkenin en güzel avantajı birden fazla değer girdiğimizde hepsinin güncellenmesi icap ederse değiştirmemize yarar
 
-/* else if birden fazla koşul atamak için kullanılır */
-else if (onunYasi === benimYasim){
-    console.log("onunla yaşıtız")
-} /* bu durumda ise yaşlarımızın eşit olduğu durumda onunla yaşıtız ifadesi gelir */
 
-/* else ise sonuç false geldiğinde yani if'deki değer karşılanmadığında bana girdiğim değeri verir */
-else{
-    console.log("ben ondan büyüğüm")
-} /* burda cevap olumsuz yani yanlış olursa bu değeri döndürmesini istedim */
+//değişken isimleri belli bir kurala göre tanımlanır
 
-/* burda ise iç içe koşul kullandık yani iki koşulda doğruysa if çalışır ama koşullardan birisi ya da ikisi yanlışsa false çalışır */
-let sayi1 = 10, sayi2 = 20;
-if (sayi1 > sayi2) {
+/* 1- değikenler sayı ile başlayamaz */
 
-    if (sayi1 > 20)
-        console.log("Sayı1 hem sayi 2den hem de 20den büyük");
-}
+/*let 1sayı yerine */ let sayı1 /* kullanılır */
 
-else {
-    console.log("Else çalıştır");
+/* 2- değişkenlerde "_" ile "$" dışında sembol kullanılamaz */
 
-}
+let _sayı1 = 123456678;
 
-let asalSayiMi = true;
-// if (asalSayiMi == true) {
+/* tercihen değişkenin ilk kelimesindeki ilk harfi küçük diğer kelimelerde ilk harf büyük olur */
 
-// } yerine ;
+let benimDogumYılım = 2003;
 
-// bu şekilde yazabiliriz ki asalSayiMi değeri true olduğu için o zaten true olarak sayılacaktır. yani true olarak belirtmemize gerek yok
-if (asalSayiMi) { }
+let benim_dogum_yılım = 2003;
 
-let a = 5, b = 15, c = 0;
+let benim_dogumYılım = 2003;
 
-if (a > b) {
-    c = a + b;
-} else { 
-    c = a - b;
-}
-console.log("c değeri:" + c);
+
+// const ile tanımladığımız değer ileride güncellenemez
+
+const piSayısı = 3.15
+
+// piSayısı = 3.14 olamaz çünkü değer değiştirilemez ve hata verir
